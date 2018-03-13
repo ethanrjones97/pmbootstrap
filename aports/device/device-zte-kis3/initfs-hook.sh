675ba39d207ec1a929ec027f7e48ae37b85d8cac
Ethan R. Jones <ethanrjones97@gmail.com>
2018-03-12 23:16:37 -0400

    device-zte-kis3: acutal initfs hook.

---
+++ aports/device/device-zte-kis3/initfs-hook.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Set the backlight to MAX intensity FIXME
+cat /sys/class/leds/wled-backlight/max_brightness > /sys/class/leds/wled-backlight/brightness
+
+# Set to ""proper"" HxW
+echo "480,1600" > /sys/class/graphics/fb0/virtual_size
+
